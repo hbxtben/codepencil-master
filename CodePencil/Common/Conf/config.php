a054aaadb37964bef351f360234dacb4bca73513
JorBen <Ben@zhangzipengdeMacBook-Pro.local>
2017-02-22 16:13:46 +0800

    first commit,finish version

---
+++ CodePencil/Common/Conf/config.php
@@ -0,0 +1,21 @@
+<?php
+return array(
+	//'é…ç½®é¡?=>'é…ç½®å€?
+	'DB_TYPE' => '',
+	'DB_HOST' => '127.0.0.1',
+	'DB_NAME' => 'codepencil',
+	'DB_USER' => '',
+	'DB_PWD' => '',
+	'DB_PORT' => '',
+	'DB_CHARSET' => 'utf8',
+
+    'URL_MODEL' => 1,
+	/**
+	 *é€šè¿‡è·¯ç”±çš„å®Œå…¨é—­åŒ…è§„åˆ™è¿›è¡ŒURLä¿æŠ¤
+	 */
+	// 'URL_ROUTER_ON' => ture,
+	// 'URL_ROUTE_RULES' => array(
+ //        'Index/:id' => 'Home/Index/index/id/2'
+	// ),
+	
+);
